cb9be04ee69f184ef10e0cfa514f1f45654afaae
Ankit Sharma <ankitzsharma252004@gmail.com>
2025-10-03 10:01:02 +0530

    Authetication Funcitonality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,23 @@ import {Route, Routes, BrowserRouter as Router} from 'react-router-dom';
 import LandingPage from './pages/landing';
 import Authentication from './pages/authentication';
 import './App.css';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
     <> 
       <Router>
 
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          {/* Add more routes here as needed */}
-          <Route path='/auth' element={<Authentication />} />  
-        </Routes>
+        <AuthProvider>
+
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            {/* Add more routes here as needed */}
+            <Route path='/auth' element={<Authentication />} />  
+          </Routes>
+
+        </AuthProvider>
+
       </Router>
     
     </>
